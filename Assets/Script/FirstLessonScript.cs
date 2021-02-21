9b9764063ffa054c1fc9ab882308c6ae9bca2849
Alper <alperctepe@gmail.com>
2021-02-21 17:50:06 +0300

    +created: first lesson scirpt

---
+++ Assets/Script/FirstLessonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+        public int tamSayi;
+        [SerializeField] int tamSayi2;
+    }
+}
+
+
