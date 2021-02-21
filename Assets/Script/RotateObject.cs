fbe92eed5631eeaa3f35216967a0257c355e5d08
Alper <alperctepe@gmail.com>
2021-02-21 20:45:14 +0300

    +added: new rotate script

---
+++ Assets/Script/RotateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+    }
+
+}
