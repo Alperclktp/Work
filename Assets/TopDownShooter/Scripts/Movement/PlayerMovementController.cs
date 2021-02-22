4a7cf598f35f112d28ebd2dc2356725bf9076478
Alper <alperctepe@gmail.com>
2021-02-22 22:51:43 +0300

    no message

---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopDownShooter.PlayerInput;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovement
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private Rigidbody _rigigbody;
+        [SerializeField] private PlayerMovementSettings _playerMovementSettings;
+
+        private void Update()
+        {
+            _rigigbody.MovePosition(_rigigbody.position + (_rigigbody.transform.forward * _inputData.vertical *
+                _playerMovementSettings.verticalSpeed));
+
+            _rigigbody.MovePosition(_rigigbody.position + (_rigigbody.transform.right * _inputData.horizontal *
+                _playerMovementSettings.horizontalSpeed));
+        }
+
+    }
+}
