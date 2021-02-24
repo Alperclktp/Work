bb5c40e043eaad8dbac9f58da6d80e9f7baad3cd
Alper <alperctepe@gmail.com>
2021-02-24 20:49:06 +0300

    no message

--- Assets/TopDownShooter/Scripts/PlayerControl/Movement/PlayerMovementController.cs
+++ Assets/TopDownShooter/Scripts/PlayerControl/Movement/PlayerMovementController.cs
@@ -7,14 +7,17 @@ namespace TopDownShooter.PlayerControls
 {
     public class PlayerMovementController : MonoBehaviour
     {
-        [SerializeField] private InputData _inputData;
         [SerializeField] private Rigidbody _rigigbody;
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private Transform _targetTransform;
         [SerializeField] private PlayerMovementSettings _playerMovementSettings;
 
         private void Update()
         {
             _rigigbody.MovePosition(_rigigbody.position + (_rigigbody.transform.forward * _inputData.vertical *
                 _playerMovementSettings.verticalSpeed));
+
+            _targetTransform.Rotate(0,_inputData.horizontal * _playerMovementSettings.horizontalSpeed, 0, Space.Self);
 
             _rigigbody.MovePosition(_rigigbody.position + (_rigigbody.transform.right * _inputData.horizontal *
                 _playerMovementSettings.horizontalSpeed));
